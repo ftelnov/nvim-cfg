38c0c7f1e530914cb3fad425b6ea545ab438d91d
Fedor Telnov <ftelnov@protonmail.com>
2023-12-13 18:23:11 +0300

    add smart history for telescope

--- plugins.lua
+++ plugins.lua
@@ -49,11 +49,30 @@ local plugins = {
     dependencies = {
       {
         "nvim-telescope/telescope-live-grep-args.nvim",
+        "nvim-telescope/telescope-smart-history.nvim",
+        "kkharji/sqlite.lua",
       },
     },
     config = function()
       local telescope = require "telescope"
+      local actions = require "telescope.actions"
+      local config_path = vim.fn.stdpath "config"
+      telescope.setup {
+        defaults = {
+          history = {
+            path = config_path .. "/telescope_history.sqlite3",
+            limit = 100,
+          },
+          mappings = {
+            i = {
+              ["<C-Down>"] = actions.cycle_history_next,
+              ["<C-Up>"] = actions.cycle_history_prev,
+            },
+          },
+        },
+      }
       telescope.load_extension "live_grep_args"
+      telescope.load_extension "smart_history"
     end,
   },
 
