cf3d6c62b7c779ee45567579ad0b3ec30a18bf26
Fedor Telnov <ftelnov@protonmail.com>
2024-01-12 17:21:00 +0300

    add crates.io support plugin

--- plugins/lsp.lua
+++ plugins/lsp.lua
@@ -63,4 +63,10 @@ return {
             }
         end,
     },
+    {
+        "saecki/crates.nvim",
+        event = { "BufRead Cargo.toml" },
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function() require("crates").setup() end,
+    },
 }
