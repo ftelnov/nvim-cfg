bf0fd3cb0462d391607c2f2774758c86378047e6
Fedor Telnov <ftelnov@protonmail.com>
2024-02-17 18:01:07 +0300

    fix hbac so it properly closes buffers(with unload)

--- plugins/hbac.lua
+++ plugins/hbac.lua
@@ -20,6 +20,7 @@ return {
         require("hbac").setup({
             autoclose = true, -- set autoclose to false if you want to close manually
             threshold = 6, -- hbac will start closing unedited buffers once that number is reached
+            close_command = function(buffnr) vim.cmd.bd({ args = { buffnr } }) end,
         })
     end,
 }
