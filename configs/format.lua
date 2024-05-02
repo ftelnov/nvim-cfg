a2375e650fa06a1d3c4b67b4ac3133d7522312b0
Fedor Telnov <ftelnov@protonmail.com>
2024-05-02 22:07:48 +0300

    feat: add temporary format disable commands

--- configs/format.lua
+++ configs/format.lua
@@ -35,10 +35,13 @@ local default_cfg = {
         zsh = { "shfmt" },
         fortran = { "fprettify" },
     },
-    format_after_save = {
-        timeout_ms = 500,
-        lsp_fallback = true,
-    },
+    format_after_save = function(bufnr)
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+            return
+        end
+        return { timeout_ms = 500, lsp_fallback = true }
+    end,
 }
 
 return {
--- init.lua
+++ init.lua
@@ -20,6 +20,24 @@ autocmd("BufWritePost", {
     callback = function() require("lint").try_lint() end,
 })
 
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+    if args.bang then
+        -- FormatDisable! will disable formatting just for this buffer
+        vim.b.disable_autoformat = true
+    else
+        vim.g.disable_autoformat = true
+    end
+end, {
+    desc = "Disable autoformat-on-save",
+    bang = true,
+})
+vim.api.nvim_create_user_command("FormatEnable", function()
+    vim.b.disable_autoformat = false
+    vim.g.disable_autoformat = false
+end, {
+    desc = "Re-enable autoformat-on-save",
+})
+
 -- autocmd("BufWritePost", {
 --   callback = function(ev)
 --     local state = require "hbac.state"
